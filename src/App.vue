4a44e584e025134e87e71dc88b57bbca734359c7
jhonnSB <jonathan@shockbyte.com>
2024-06-02 23:51:25 +0200

    :lipstick: updates Home button styles

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div id="app">
     <header class="flex items-center bg-gray-800 text-white">
       <nav>
-        <router-link to="/"><h2 class="p-8 bg-gray-600">Home</h2></router-link>
+        <router-link to="/"><h2 class="p-8 border-r-2 border-cmt-blue text-cmt-blue">Home</h2></router-link>
       </nav>
       <h1 class="p-7 text-center text-2xl lato-bold">Client Management Tool</h1>
     </header>
-    <main class="flex justify-center p-10 ">
+    <main class="flex justify-center p-10">
       <router-view />
     </main>
   </div>
